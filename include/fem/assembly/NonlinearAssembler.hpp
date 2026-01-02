448c585b8a2a39c44b206056e9f52ec5a1ee3255
Eli Case <elicase4@gmail.com>
2026-01-01 23:32:09 -0600

    edit: fem-core

---
+++ include/fem/assembly/NonlinearAssembler.hpp
@@ -0,0 +1,40 @@
+#ifndef PDESOLVER_LINEARASSEMBLER_HPP
+#define PDESOLVER_LINEARASSEMBLER_HPP
+
+#include "core/Types.hpp"
+#include "mesh/MeshBase.hpp"
+#include "linalg/types/SparseMatrix.hpp"
+#include "linalg/types/Vector.hpp"
+
+namespace pdesolver {
+	namespace fem {
+		namespace assembly {
+			
+			template<typename Element, typename Quadrature, typename Backend>
+			class NonlinearAssembler{
+			public:
+				
+				// allocation functions
+				static linalg::types::SparseMatrix<Real, Backend> createNonlinearTangentMatrixSystem(const mesh::MeshBase& mesh);
+				static linalg::types::Vector<Real, Backend> createNonlinearTangentOperatorSystem(const mesh::MeshBase& mesh);
+				static linalg::types::Vector<Real, Backend> createResidualVector(const mesh::MeshBase& mesh);
+				
+				// matrix assembly
+				template<typename NonlinearTangentForm>
+				static void assembleNonlinearTangentMatrixSystem(const mesh::MeshBase& mesh, linalg::types::SparseMatrix<Real, Backend>& K);
+				
+				// operator assembly
+				template<typename NonlinearTangentForm>
+				static void assembleNonlinearTangentOperatorSystem(const mesh::MeshBase& mesh, linalg::types::Vector<Real, Backend>& O);
+				
+				// vector assembly
+				template<typename NonlinearForm>
+				static void assembleResidualVector(const mesh::MeshBase& mesh, linalg::types::Vector<Real, Backend>& R);
+
+			}; // class NonlinearAssember
+
+		} // namespace assembly
+	} // namespace fem
+} // namespace pdesolver
+
+#endif
