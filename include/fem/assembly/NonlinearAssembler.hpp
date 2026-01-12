cde63f82fe8c46c1208677154fbd7184cb8d0574
Eli Case <elicase4@gmail.com>
2026-01-11 23:31:56 -0600

    edit: bc structure

--- include/fem/assembly/NonlinearAssembler.hpp
+++ include/fem/assembly/NonlinearAssembler.hpp
@@ -17,21 +17,21 @@ namespace pdesolver {
 			public:
 				
 				// allocation functions
-				static linalg::types::SparseMatrix<Real, Backend> createNonlinearTangentMatrixSystem(const mesh::Mesh& mesh, const Index DofsPerNode);
-				static linalg::types::Vector<Real, Backend> createNonlinearTangentOperatorSystem(const mesh::Mesh& mesh, const Index DofsPerNode);
-				static linalg::types::Vector<Real, Backend> createResidualVector(const mesh::Mesh& mesh, const Index DofsPerNode);
+				static linalg::types::SparseMatrix<Real, Backend> createNonlinearTangentMatrixSystem(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF);
+				static linalg::types::Vector<Real, Backend> createNonlinearTangentOperatorSystem(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF);
+				static linalg::types::Vector<Real, Backend> createResidualVector(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF);
 				
 				// matrix assembly
 				template<typename NonlinearTangentForm>
-				static void assembleNonlinearTangentMatrixSystem(const mesh::Mesh& mesh, const Index DofsPerNode, linalg::types::SparseMatrix<Real, Backend>& K);
+				static void assembleNonlinearTangentMatrixSystem(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF, linalg::types::SparseMatrix<Real, Backend>& K);
 				
 				// operator assembly
 				template<typename NonlinearTangentForm>
-				static void assembleNonlinearTangentOperatorSystem(const mesh::Mesh& mesh, const Index DofsPerNode, linalg::types::Vector<Real, Backend>& O);
+				static void assembleNonlinearTangentOperatorSystem(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF, linalg::types::Vector<Real, Backend>& O);
 				
 				// vector assembly
 				template<typename NonlinearForm>
-				static void assembleResidualVector(const mesh::Mesh& mesh, const Index DofsPerNode, linalg::types::Vector<Real, Backend>& R);
+				static void assembleResidualVector(const mesh::Mesh& mesh, const topology::TopologicalDOF& topoDOF, linalg::types::Vector<Real, Backend>& R);
 
 			}; // class NonlinearAssember
 
