531be94f0c1619f7f8bfc8deb62a27f7c5ff0ac7
Eli Case <elicase4@gmail.com>
2026-04-12 23:27:36 -0500

    add: linalg operator

---
+++ include/linalg/operator/FEMOperator.hpp
@@ -0,0 +1,14 @@
+#ifndef PDESOLVER_LINALG_FEMOPERATOR_HPP
+#define PDESOLVER_LINALG_FEMOPERATOR_HPP
+
+namespace pdesolver {
+	namespace linalg {
+		namespace op {
+
+
+
+		} // namespace op
+	} // namespace linalg
+} // namespace pdesolver
+
+#endif
